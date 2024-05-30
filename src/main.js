2bda7c9136ec2408384f8ac8ef0c3142e4c8bfba
mioe <mioe.boss@gmail.com>
2024-05-30 20:17:19 +0300

    init eslint && set default template

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+
+/**
+ * Styles
+ */
+import 'uno.css'
+import './style.css'
 
 createApp(App).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Unocss from 'unocss/vite'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ command, mode }) => {
+	console.log('🦕 vite.config.js/defineConfig', command, mode)
+
+	return {
+		base: mode === 'development' ? './' : '/json-path-picker/',
+
+		resolve: {
+			alias: {
+				'~/': `${path.resolve(__dirname, 'src')}/`,
+			},
+		},
+
+		plugins: [
+			vue(),
+
+			// https://github.com/unocss/unocss
+			Unocss(),
+		],
+	}
 })
